21e24642509bb6ce730341ed2be77b0e39355f85
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-06-17 09:08:49 +0300

    Update config.js

--- Robotic/static/js/common/config.js
+++ Robotic/static/js/common/config.js
@@ -1,6 +1,7 @@
 // Central configuration for API and environment values.
 window.SmartAIConfig = {
-  API_URL: "http://localhost:5000/api",
+  API_URL: "/api",
+  CHAT_API_URL: null, // set to a real backend chat endpoint when available
   APP_NAME: "SmartAI",
   ENABLE_LIVE_DETECTION_API: false,
 };
