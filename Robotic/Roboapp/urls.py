962fb5bbde7af9fa839116e2d35bda7ed25dc41c
Douglas Mwebi <mwebidouglas08@gmail.com>
2026-06-17 08:59:01 +0300

    Update urls.py

--- Robotic/Roboapp/urls.py
+++ Robotic/Roboapp/urls.py
@@ -6,21 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
- main
-    path('register/', views.register, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('forgot-password/', views.forgot_password, name='forgot-password'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('analytics/', views.analytics, name='analytics'),
-    path('diseasedetection/', views.diseasedetection, name='diseasedetection'),
-    path('farmsimulation/', views.farmsimulation, name='farmsimulation'),
-    path('treatmenthistory/', views.treatmenthistory, name='treatmenthistory'),
-    path('reports/', views.reports, name='reports'),
-    path('profile/', views.profile, name='profile'),
-    path('settings/', views.settings, name='settings'),
-]
-=======
     path('analytics/', views.analytics, name='analytics'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('diseasedetection/', views.diseasedetection, name='diseasedetection'),
@@ -34,4 +19,3 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.logout_user, name='logout'),
 ]
- main
